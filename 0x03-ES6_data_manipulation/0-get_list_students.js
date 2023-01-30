e6b923437d52495d2a3e6c9a663135af8ef601fd
phiona-b3 <phiona030303@gmail.com>
2023-01-30 21:15:52 +0000

    Task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 export default function getListStudents(){
-    const array1 = [
-        {id: 1, firstName: "Guillaume", location: "San Francisco"},
-        {id: 2, firstName: "James", location: "Columbia"},
-        {id: 5, firstName: "Serena", location: "San Francisco"},
-    ];
+  const array1 = [
+      {id: 1, firstName: "Guillaume", location: "San Francisco"},
+      {id: 2, firstName: "James", location: "Columbia"},
+      {id: 5, firstName: "Serena", location: "San Francisco"},
+  ];
 
-    return array1;
+  return array1;
 }
