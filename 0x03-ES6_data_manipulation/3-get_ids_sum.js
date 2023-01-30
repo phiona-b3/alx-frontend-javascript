9e706841bdf05b06547aae20fd7969e6defe7a5b
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2023-01-31 00:29:17 +0300

    Task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(list) {
-    return list.reduce((currentTotal, obj) => obj.id + currentTotal, 0);
+  return list.reduce((currentTotal, obj) => obj.id + currentTotal, 0);
 }
